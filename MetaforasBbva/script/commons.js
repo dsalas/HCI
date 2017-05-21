4b791399565fe1c1c8d85942810569755541d360
DIA <DIA@MacBook-Air-de-DIA.local>
2017-05-21 18:24:09 -0500

    Avance venta entradas

--- MetaforasBbva/script/commons.js
+++ MetaforasBbva/script/commons.js
@@ -9,6 +9,17 @@ $(function() {
             window.location = href;
         }
     });
+    
+    $('.button-container').hover(function() {
+        $(this).css('cursor','pointer');
+    });
+    
+   $('.button-container').click(function() {
+        var href = $(this).find("a").attr("href");
+        if(href) {
+            window.location = href;
+        }
+    });
 });
 
 function createSearchTool(container) {
@@ -18,3 +29,18 @@ function createSearchTool(container) {
 		$(container).append(letterContainer);
 	});
 }
+
+var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : sParameterName[1];
+        }
+    }
+};	
